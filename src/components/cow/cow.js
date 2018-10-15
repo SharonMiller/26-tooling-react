ee627e10a1918ccc4a599019ad5b3c9a6ad4ec4c
Sharon Miller <sharon@sharonmillercreative.com>
2018-10-15 16:31:54 -0700

    watch run working

---
+++ src/components/cow/cow.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+import './cow.scss';
+
+class Cow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app.js';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <App />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
